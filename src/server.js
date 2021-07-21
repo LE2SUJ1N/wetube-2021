971d9c19b369b74cfca75bfb81ca328eb3489580
LE2SUJ1N <84925853+LE2SUJ1N@users.noreply.github.com>
2021-07-22 01:11:12 +0900

    wetube-210722

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
+
 const privateMiddleware = (req, res, next) => {
   const url = req.url;
   if (url === "/protected") {
@@ -21,14 +22,9 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the pricate lounge.");
-};
-
 app.use(logger);
 app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
