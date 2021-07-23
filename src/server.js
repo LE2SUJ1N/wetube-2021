887b5889171f9ba80315e9a6887afce3fc46eaae
LE2SUJ1N <84925853+LE2SUJ1N@users.noreply.github.com>
2021-07-23 11:59:03 +0900

    update-210723

--- src/server.js
+++ src/server.js
@@ -3,28 +3,29 @@ import express from "express";
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev"); //이거뭔데?
+app.use(logger);
+
+const glabalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+glabalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUsesr = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUsesr);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", glabalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
