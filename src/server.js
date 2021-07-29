8b477d099e5cf575aa3cb5fd115563332e4a41ec
LE2SUJ1N <84925853+LE2SUJ1N@users.noreply.github.com>
2021-07-29 23:58:40 +0900

    update-210729

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4002;
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev"); //이거뭔데?
@@ -12,6 +12,7 @@ const logger = morgan("dev"); //이거뭔데?
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
